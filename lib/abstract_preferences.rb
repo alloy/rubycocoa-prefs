6879533c9ef764ba7dad565fa64f150f856c05ee
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-07 15:11:09 +0100

    Removed #to_ruby calls and worked around seg faults with Manfreds excellent NSUserDefaults test helper, need to add that to Rucola.

--- lib/abstract_preferences.rb
+++ lib/abstract_preferences.rb
@@ -70,7 +70,7 @@ class Preferences
         
         class_eval do
           define_method(name) do
-            Preferences.user_defaults[key_path].to_ruby
+            Preferences.user_defaults[key_path]
           end
           
           define_method("#{name}=") do |value|
@@ -141,7 +141,7 @@ class Preferences
       attr_accessor :key_path
       
       def array
-        Preferences.user_defaults[key_path].to_ruby
+        Preferences.user_defaults[key_path]
       end
       
       def array=(array)
@@ -261,7 +261,7 @@ class Preferences
         def observeValueForKeyPath_ofObject_change_context(key_path, observed, change, context)
           value_key_path = key_path.sub(/^values\./, '')
           callback_method = "#{key_path.split('.').last}_changed"
-          send(callback_method, Preferences.user_defaults[value_key_path].to_ruby)
+          send(callback_method, Preferences.user_defaults[value_key_path])
         end
       end
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,44 @@ require 'rucola/test_spec'
 require 'mocha'
 
 framework 'Cocoa'
+
+
+# Without this helper we get segfaults, probably because the objects stored
+# in the defaults are not really being maintained during the tests.
+require 'singleton'
+class UserPreferences
+  include Singleton
+  
+  def initialize
+    reset!
+  end
+  
+  def registerDefaults(defaults)
+    @defaults = defaults
+  end
+  
+  def [](key)
+    @defaults.merge(@preferences)[key]
+  end
+  alias_method :objectForKey, :[]
+  
+  def []=(key, value)
+    @preferences[key] = value
+  end
+  alias_method :setObjectForKey, :[]=
+  
+  def synchronize
+  end
+  
+  def reset!
+    @preferences = {}
+    @defaults = {}
+  end
+end
+
+# We don't want test ruining our preferences
+class NSUserDefaults < NSObject
+  def self.standardUserDefaults
+    UserPreferences.instance
+  end
+end
